c5e7431f6e870ef7a8cf0319333787d1d8b7b6d9
francobenitez <franco.benitez@hogarthww.com>
2022-01-24 10:56:43 -0300

    init

--- builds/d3/js/script.js
+++ builds/d3/js/script.js
@@ -1,1 +1,39 @@
-d3.selectAll("tr:nth-child(even) .day-high .temp").text("hot");
+// We add html inside the selected element
+d3.selectAll("tr:nth-child(1) .day-high .temp")
+  .html("<strong>hot</strong>");
+
+// We add html inside the selected element
+// and we add a new span element with the 'today' text
+d3.selectAll("tr:nth-child(1) .day-high .temp")
+  .html("<strong>hot</strong>")
+  .append('span')
+  .html(' today')
+
+// We add html inside the selected element
+// and we add a new span element with the 'today' text
+// in a specific position
+d3.selectAll("tr:nth-child(1) .day-high .temp")
+  .html("<strong>hot</strong>")
+  .insert('span', ':first-child')
+  .html('today ')
+
+// We add html inside the selected element
+// and we add a new span element with the 'today' text
+// in a specific position
+// and add the 'item' class to the HTML
+d3.selectAll("tr:nth-child(1) .day-high .temp")
+  .html('<strong class="item">hot</strong>')
+  .insert('span', ':first-child')
+  .html('today ')
+
+// We add html inside the selected element
+// and we add a new span element with the 'today' text
+// before the 'strong' tag
+d3.selectAll("tr:nth-child(1) .day-high .temp")
+  .html('<strong>hot</strong>')
+  .insert('span', 'strong')
+  .html('today ')
+
+// Remove the element 5 from the table
+d3.selectAll('tr:nth-child(5)')
+  .remove()
