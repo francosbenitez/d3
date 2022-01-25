d0247f07577d34b509bda9944df8b1549a19d367
francobenitez <franco.benitez@hogarthww.com>
2022-01-25 09:54:33 -0300

    init

--- builds/d3/js/script.js
+++ builds/d3/js/script.js
@@ -1,23 +1,43 @@
-d3.selectAll("tr:nth-child(4) .day-high")
-  .append('span')
-  .html('hot')
+// This allows to change the first value inside
+// the 'High' cateogory for the number 45 
+// Also, it prints the data in console
+// d3.selectAll(".day-high .temp")
+//   .data([45])
+//   .html(function(d) {
+//     console.log(d)
+//     return '<strong>' + d + '</strong>'
+//   })
 
-  // .style('background', 'red')
-  // .style('padding', '3px')
-  // .style('margin-left', '3px')
-  // .style('border-radius', '3px')
-  // .style('color', 'white')
+// The same as before, but with arrow function
+// d3.selectAll(".day-high .temp")
+//   .data([45])
+//   .html(d => {
+//     console.log('d from arrow function')
+//     return '<strong>' + d + '</strong>'
+//   })
 
-  .classed('label label-danger', true)
-  .classed('label-danger', false)
-  .classed('label-warning', true)
-  .style('margin-left', '3px')
+// This changes all of the items inside the 
+// 'High' category for the new data put in the 
+// data() function
+// d3.selectAll(".day-high .temp")
+//   .data([45, 78, 77, 66, 76])
+//   .html(function(d) {
+//     console.log(d)
+//     return '<strong>' + d + '</strong>'
+//   })
 
-d3.select('h2 span')
-  .classed('small', false)
-
-d3.selectAll('tr')
-  .insert('td', ':first-child')
-  .append('input')
-  .attr('type', 'checkbox')
-  .property('checked', true)
+// The same as before, but with more complex conditions added
+d3.selectAll(".day-high .temp")
+  .data([45, 78, 77, 66, 76])
+  .html(function(d, i) {
+    if (i == 0) {
+      console.log('from index', i, d)
+      return '<strong class="text-muted" style="font-size: 2rem;">' + d + '</strong>'
+    } else {
+      if (d >= 76) {
+        return '<strong class="btn-danger">' + d + '</strong>'
+      } else {
+        return '<strong>' + d + '</strong>'
+      }
+    }
+  })
