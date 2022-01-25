b086861c9795d9383054ce25b7a81e15939daeab
francobenitez <franco.benitez@hogarthww.com>
2022-01-25 10:56:38 -0300

    init

--- builds/d3/js/script.js
+++ builds/d3/js/script.js
@@ -1,13 +1,44 @@
-d3.selectAll(".day-high .temp")
-  .data([45, 78, 77, 66, 76])
-  .html(function(d, i) {
-    if(i==0) {
-      return '<strong class="text-muted" style="font-size: 2rem">' + d + '</strong>';
-    } else {
-      if (d >= 77) {
-        return '<strong class="text-danger">' + d + '</strong>';
-      } else {
-        return '<strong>' + d + '</strong>';
-      }
-    }
+var myData = [
+  {
+    date: '4/01/2017',
+    low: 55,
+    high: 78
+  }, 
+  {
+    date: '4/02/2017',
+    low: 65,
+    high: 83
+  }, 
+  {
+    date: '4/03/2017',
+    low: 77,
+    high: 90
+  }, 
+  {
+    date: '4/04/2017',
+    low: 58,
+    high: 78
+  }, 
+  {
+    date: '4/05/2017',
+    low: 67,
+    high: 92
+  }, 
+]
+
+// Go to the 'tbody' and find something called 'selectAll'
+d3.select('tbody')
+
+  // This selects something that doesn't exist yet 
+  // in the 'tbody' tag
+  .selectAll('tr')
+  .data(myData)
+
+  // Insert the 'tr' in the body! 
+  .enter().append('tr')
+
+  .html(function(d) {
+    return '<th scope="row">' + d.date + '</th>' +
+            '<td>' + d.low + '</td>' +
+            '<td>' + d.high + '</td>'
   })
