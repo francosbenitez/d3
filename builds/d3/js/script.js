82163b9c34d0928121c73e3d8df5496399a5f7a2
francobenitez <franco.benitez@hogarthww.com>
2022-01-25 09:22:17 -0300

    init

--- builds/d3/js/script.js
+++ builds/d3/js/script.js
@@ -1,7 +1,38 @@
-d3.selectAll("tr:nth-child(1) .day-high .temp")
-  .html("<strong>hot</strong>")
-  .insert('span', 'strong')
-  .html('today ');
+// Here, we use the 'style' method
+d3.selectAll("tr:nth-child(4) .day-high")
+  .append('span')
+  .html('hot')
+  .style('background', 'red')
+  .style('padding', '3px')
+  .style('margin-left', '3px')
+  .style('border-radius', '3px')
+  .style('color', 'white')
 
-  d3.selectAll('tr:nth-child(3)')
-    .remove();
+// Here, we use the 'classed' method with 
+// the Bootstrap framework
+d3.selectAll("tr:nth-child(4) .day-high")
+  .append('span')
+  .html('hot')
+  .classed('label label-danger', true)
+  .classed('label-danger', false)
+  .classed('label-warning', true)
+  .style('margin-left', '3px')
+
+// This removes the 'small' class from 'span' tag
+d3.select('h2 span')
+  .classed('small', false)
+
+// This adds a serie of checkbox on the left 
+// of the columns
+d3.selectAll('tr')
+  .insert('td', ':first-child')
+  .append('input')
+  .attr('type', 'checkbox')
+
+// This adds a serie of checkbox checked
+// on the left of the columns
+d3.selectAll('tr')
+  .insert('td', ':first-child')
+  .append('input')
+  .attr('type', 'checkbox')
+  .property('checked', true)
