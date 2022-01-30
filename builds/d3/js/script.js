f87c86fc72a53a36e9a8b3d728938445110263d3
francobenitez <franco.benitez@hogarthww.com>
2022-01-29 22:25:04 -0300

    init

--- builds/d3/js/script.js
+++ builds/d3/js/script.js
@@ -1,23 +1,27 @@
-var bardata = [20, 30, 45, 15];
+var barData = [20, 30, 45, 15];
 var height = 400,
     width = 600,
     barWidth = 50,
     barOffset = 5;
 
+var yScale = d3.scaleLinear()
+               .domain([0, d3.max(barData)])
+               .range([0, height])
+
 d3.select('#viz').append('svg')
   .attr('width', width)
   .attr('height', height)
   .style('background', '#C9D7D6')
-.selectAll('rect').data(bardata)
+.selectAll('rect').data(barData)
   .enter().append('rect')
     .style('fill', '#C61C6F')
     .attr('width', barWidth)
     .attr('height', function(d) {
-      return d;
+      return yScale(d);
     })
     .attr('x', function(d, i) {
-      return i*(barWidth + barOffset)
+      return i * (barWidth + barOffset)
     })
     .attr('y', function(d) {
-      return height - d;
+      return height - yScale(d);
     });
